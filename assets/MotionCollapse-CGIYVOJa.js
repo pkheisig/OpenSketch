71b4d36ac7dfc3e4718581e469101a499c78ac74
Paul Heisig <pkheisig@gmail.com>
2026-08-08 14:32:47 +0200

    Deploy OpenSketch build eb0caa5e

---
+++ assets/MotionCollapse-CGIYVOJa.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-wxKs8x_V.js";function t({open:i,children:s,className:e=""}){return o.jsx("div",{className:`motion-collapse ${i?"open":""} ${e}`.trim(),"aria-hidden":!i,inert:i?void 0:!0,children:o.jsx("div",{className:"motion-collapse-inner",children:s})})}export{t as M};
